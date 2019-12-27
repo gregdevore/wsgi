e8b435f93c7b2766def9db353dbd018eec072ce2
Greg DeVore <greg.h.devore@gmail.com>
2019-12-27 12:51:29 -0800

    Completed bookapp exercise

--- bookapp.py
+++ bookapp.py
@@ -1,4 +1,5 @@
 import re
+import traceback
 
 from bookdb import BookDB
 
@@ -6,16 +7,64 @@ DB = BookDB()
 
 
 def book(book_id):
-    return "<h1>a book with id %s</h1>" % book_id
-
+    book = DB.title_info(book_id)
+    if not book:
+        raise NameError
+    body = """
+    <h1>{title}</h1>
+    <table>
+        <tr><th align="left">Author</th><td>{author}</td></tr>
+        <tr><th align="left">Publisher</th><td>{publisher}</td></tr>
+        <tr><th align="left">ISBN</th><td>{isbn}</td></tr>
+    </table>
+    <a href="/">Return to all books</a>
+    """.format(**book)
+    return body
 
 def books():
-    return "<h1>a list of books</h1>"
+    all_books = DB.titles()
+    body = ['<h1>My Library</h1>', '<ul>']
+    book_template = '<li><a href="/book/{id}">{title}</a></li>'
+    for book in all_books:
+        body.append(book_template.format(**book))
+    body.append('</ul>')
 
+    return '\n'.join(body)
+
+def resolve_path(path):
+    functions = {'':books,
+                 'book':book}
+    path = path.strip('/').split('/')
+    func_name = path[0]
+    args = path[1:]
+    try:
+        func = functions[func_name]
+    except KeyError:
+        raise NameError
+
+    return func, args
 
 def application(environ, start_response):
-    status = "200 OK"
     headers = [('Content-type', 'text/html')]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if not path:
+            raise NameError
+        func, args = resolve_path(path)
+        status = "200 OK"
+        body = func(*args)
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length',str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+
     start_response(status, headers)
     return ["<h1>No Progress Yet</h1>".encode('utf8')]
 
